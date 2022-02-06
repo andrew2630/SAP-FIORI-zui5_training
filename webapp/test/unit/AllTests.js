a050d9d089a2f2400c6f6475c59b766fbf8a6e41
andrew2630 <kontakt@andrzejpudzisz.com>
2022-02-06 13:13:06 +0100

    init

---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ui5training./zui5_training/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
---
+++ webapp/test/unit/unitTests.qunit.js
@@ -0,0 +1,12 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"ui5training./zui5_training/test/unit/AllTests"
+	], function () {
+		QUnit.start();
+	});
+});
