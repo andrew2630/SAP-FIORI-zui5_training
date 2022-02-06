d18293c83f20c677329e87849f605625fd732c38
andrew2630 <kontakt@andrzejpudzisz.com>
2022-02-06 13:37:52 +0100

    przykładowa funkcjonalność

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,13 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
-
-        return Controller.extend("ui5.training.zui5training.controller.App", {
-            onInit: function () {
-
-            }
+sap.ui.define(
+  ['sap/ui/core/mvc/Controller', 'sap/m/MessageBox', 'sap/m/MessageToast'],
+  (Controller, MessageBox, MessageToast) => {
+    'use strict';
+    return Controller.extend('ui5.training.zui5training.controller.App', {
+      onInit() {
+        MessageBox.alert(`Hello there!`, {
+          onClose: () => MessageToast.show(`General Kenobi`),
         });
+      },
     });
+  }
+);
